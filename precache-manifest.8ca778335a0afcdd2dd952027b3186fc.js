6f78c96aefa862307555162f0b1e00d6db973c87
josielsifa <josielsifa@gmail.com>
2019-10-17 22:13:32 -0300

    Updates

---
+++ precache-manifest.8ca778335a0afcdd2dd952027b3186fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2f2d82f3a3981a7dc2a4cbd44fbb655",
+    "url": "/challenge-enext/index.html"
+  },
+  {
+    "revision": "2221987ddf12b92d2d71",
+    "url": "/challenge-enext/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "73d09e008297ba94de80",
+    "url": "/challenge-enext/static/css/main.5126a353.chunk.css"
+  },
+  {
+    "revision": "2221987ddf12b92d2d71",
+    "url": "/challenge-enext/static/js/2.7677faf5.chunk.js"
+  },
+  {
+    "revision": "73d09e008297ba94de80",
+    "url": "/challenge-enext/static/js/main.576747ed.chunk.js"
+  },
+  {
+    "revision": "522dda8a5a63d96d6790",
+    "url": "/challenge-enext/static/js/runtime-main.5c6b2e1c.js"
+  }
+]);
