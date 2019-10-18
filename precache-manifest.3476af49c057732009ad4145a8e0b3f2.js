ae48ef23aea33eff209d3d32819f007fbb05acbc
Josiel Faria <josiel@farmaservice.com.br>
2019-10-18 09:51:15 -0300

    Updates

---
+++ precache-manifest.3476af49c057732009ad4145a8e0b3f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3b62de24579ff149387fe9efbea0766",
+    "url": "/challenge-enext/index.html"
+  },
+  {
+    "revision": "2221987ddf12b92d2d71",
+    "url": "/challenge-enext/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "72451ad0f852fabfc07e",
+    "url": "/challenge-enext/static/css/main.60343486.chunk.css"
+  },
+  {
+    "revision": "2221987ddf12b92d2d71",
+    "url": "/challenge-enext/static/js/2.7677faf5.chunk.js"
+  },
+  {
+    "revision": "72451ad0f852fabfc07e",
+    "url": "/challenge-enext/static/js/main.98ad69f5.chunk.js"
+  },
+  {
+    "revision": "522dda8a5a63d96d6790",
+    "url": "/challenge-enext/static/js/runtime-main.5c6b2e1c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/challenge-tnl/precache-manifest.0208e8a11a1e9260746c30da7b6d2a9b.js"
+  "/challenge-enext/precache-manifest.3476af49c057732009ad4145a8e0b3f2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/challenge-tnl/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/challenge-enext/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
